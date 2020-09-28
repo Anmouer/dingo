3c663879237d8cb23a10dfe1057b88f5850539f1
Anmouer <55122038+Anmouer@users.noreply.github.com>
2020-09-28 13:43:35 +0800

    Update UserController.php
    コントローラ修正

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
-use Illuminate\Routing\Controller as BaseController;
 
 class UserController extends BaseController
 {
